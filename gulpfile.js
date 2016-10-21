10b40bd86372d3317561d8037c1b3d5b5b4440fe
wangdan <2922752652@qq.com>
2016-10-21 15:09:30 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,71 @@
-var gulp = require('gulp'),
-sass = require('gulp-sass'),
-minifycss = require('gulp-minify-css'),
-concat = require('gulp-concat'),
-uglify = require('gulp-uglify'),
-rename = require('gulp-rename');
-gulp.task('sass',function(){
-return gulp.src('sass/*.sass')//编译的文件
-    .pipe(sass())//编译
-    .pipe(gulp.dest('css'));});//输出
-gulp.task('minifycss', function() {
-return gulp.src('css/*.css')      //压缩的文件
-    .pipe(concat('main.css'))
-    .pipe(gulp.dest('minified/css'))   //输出文件夹
-    .pipe(rename({suffix: '.min'}))    //rename压缩后的文件夹
-    .pipe(minifycss())   //执行压缩
-    .pipe(gulp.dest('minified/css'));});
- gulp.task('minifyjs', function() {
-return gulp.src('js/*.js')//压缩的文件
-    .pipe(concat('main.js'))    //合并所有js到main.js
-    .pipe(gulp.dest('minified/js'))    //输出main.js到文件夹
-    .pipe(rename({suffix: '.min'}))   //rename压缩后的文件名
-    .pipe(uglify())    //压缩
-    .pipe(gulp.dest('minified/js'));//输出});
-gulp.task('default',['sass'],  function() {
-gulp.start('minifycss', 'minifyjs');});
- 
+// 引入 gulp
+var gulp = require('gulp');
+
+// 引入组件
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var less = require('gulp-less');
+var minifycss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+
+// 检查js脚本
+gulp.task('lint', function() {
+    gulp.src('./js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// 合并,压缩js文件
+gulp.task('scripts', function() {
+    gulp.src('./js/*.js')
+        //合并js文件
+        .pipe(concat('all.js'))
+        //给文件添加.min后缀
+        .pipe(rename({ suffix: '.min' }))
+        //压缩脚本文件
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
+});
+
+// 编译sass
+gulp.task('sass', function() {
+    gulp.src('./scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'));
+});
+
+// 编译less
+gulp.task('sass', function() {
+    gulp.src('./less/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('./css'));
+});
+
+// 压缩css
+gulp.task('style', function() {
+    gulp.src('./css/*.css')
+        .pipe(rename('all.min.css'))
+        .pipe(minifycss())
+        .pipe(gulp.dest('./dist/style'))
+});
+
+// 默认任务
+gulp.task('default', function(){
+    gulp.run('lint', 'sass', 'scripts','style');
+
+    // 监听文件变化
+    gulp.watch('./js/*.js', function(){
+        gulp.run('lint', 'scripts');
+    });
+    gulp.watch('./sass/*.scss', function(){
+        gulp.run('sass');
+    });
+    gulp.watch('./less/*.less', function(){
+        gulp.run('less');
+    });
+    gulp.watch('./css/*.css', function(){
+        gulp.run('style');
+    });
+});
